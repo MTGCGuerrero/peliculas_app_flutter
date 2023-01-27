25507be2742ca8b08eb6b5aafc4f91f534834420
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-01-27 15:59:23 -0300

    custom silverappbar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,10 +5,44 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Cambiar por instanciacion de movie
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? "no-movie";
+
     return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text("Details Screen"),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigoAccent,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black45,
+          child: const Text(
+            "movie.title",
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage("assets/loading.gif"),
+          image: NetworkImage("https://via.placeholder.com/500x300"),
+          fit: BoxFit.cover,
         ),
       ),
     );
