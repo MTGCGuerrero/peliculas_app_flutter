f35155b983e51ec7e2762d43eecfc315f6afb6b9
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-01-27 15:35:29 -0300

    touches to movieslider

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -7,8 +7,7 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 260,
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 20),
@@ -17,12 +16,13 @@ class MovieSlider extends StatelessWidget {
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
+        const SizedBox(height: 5),
         Expanded(
           child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 1,
-              itemBuilder: (_, int index) => _MoviePoster(),
-               ),
+            scrollDirection: Axis.horizontal,
+            itemCount: 1,
+            itemBuilder: (_, int index) => _MoviePoster(),
+          ),
         )
       ]),
     );
@@ -35,8 +35,32 @@ class _MoviePoster extends StatelessWidget {
     return Container(
       width: 130,
       height: 190,
-      color: Colors.green,
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children:  [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, "details",
+                arguments: "movie-instance"),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage("assets/no-image.jpg"),
+                image: NetworkImage("https://via.placeholder.com/300x400"),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          const Text(
+            "Peclicula re peola ameooooo",
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
     );
   }
 }
