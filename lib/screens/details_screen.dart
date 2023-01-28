2573569953d9c37b88e3ede84d8c7a27d306ea85
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-01-28 10:31:44 -0300

    add poster and title

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -13,6 +13,10 @@ class DetailScreen extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           _CustomAppBar(),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            _PosterAndTitle(),
+          ]))
         ],
       ),
     );
@@ -45,6 +49,55 @@ class _CustomAppBar extends StatelessWidget {
           fit: BoxFit.cover,
         ),
       ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  //final TextTheme textTheme = Theme.of(context).textTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: const FadeInImage(
+            placeholder: AssetImage("assets/loading.gif"),
+            image: NetworkImage("https://via.placeholder.com/200x300"),
+            height: 150,
+          ),
+        ),
+        const SizedBox(width: 20),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "movie.title",
+              style: Theme.of(context).textTheme.headline5,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+            Text(
+              "movie.originalTitle",
+              style: Theme.of(context).textTheme.subtitle1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Row(
+              children: [
+                const Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                const SizedBox(width: 5),
+                Text(
+                  "movie.voteAverage",
+                  style: Theme.of(context).textTheme.caption,
+                )
+              ],
+            )
+          ],
+        ),
+      ]),
     );
   }
 }
