16e3a1f09100867686ae92651d4d40fa99c5fb50
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-01-30 11:07:20 -0300

    popular movies

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -13,11 +13,13 @@ class MoviesProvider extends ChangeNotifier {
   final String _language = "en-US";
 
   List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
   // ignore: non_constant_identifier_names
   MoviesProvider() {
     // ignore: avoid_print
     print("MoviesProvider Inicializado");
     getOnDisplayMovies();
+    getPopularMovies();
   }
 
   getOnDisplayMovies() async {
@@ -31,6 +33,19 @@ class MoviesProvider extends ChangeNotifier {
     //final Map<String, dynamic> decodedData = json.decode(response.body);
     //print(nowPlayingResponse.results[0].title);
     onDisplayMovies = nowPlayingResponse.results;
+    notifyListeners();
+  }
+
+  getPopularMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/popular',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+    final popularResponse = PopularResponse.fromJson(jsonDecode(response.body));
+    //final Map<String, dynamic> decodedData = json.decode(response.body);
+    //print(nowPlayingResponse.results[0].title);
+    popularMovies = [...popularMovies, ...popularResponse.results];
     notifyListeners();
   }
 }
