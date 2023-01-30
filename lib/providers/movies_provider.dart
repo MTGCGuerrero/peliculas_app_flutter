8fba5d3a687b853dec79443e66d1d97d14cf495b
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-01-30 12:25:01 -0300

    optimize endpoint

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -14,6 +14,9 @@ class MoviesProvider extends ChangeNotifier {
 
   List<Movie> onDisplayMovies = [];
   List<Movie> popularMovies = [];
+
+  int _popularPage = 0;
+
   // ignore: non_constant_identifier_names
   MoviesProvider() {
     // ignore: avoid_print
@@ -22,14 +25,23 @@ class MoviesProvider extends ChangeNotifier {
     getPopularMovies();
   }
 
-  getOnDisplayMovies() async {
-    var url = Uri.https(_baseUrl, '3/movie/now_playing',
-        {'api_key': _apiKey, 'language': _language, 'page': '1'});
-
-    // Await the http get response, then decode the json-formatted response.
+  Future<String> _getJsonData(String endpoint, [int page = 1]) async {
+    var url = Uri.https(_baseUrl, endpoint,
+        {'api_key': _apiKey, 'language': _language, 'page': '$page'});
     final response = await http.get(url);
+
+    return response.body;
+  }
+
+  getOnDisplayMovies() async {
+    final jsonData = await _getJsonData('3/movie/now_playing');
+    // var url = Uri.https(_baseUrl, '3/movie/now_playing',
+    //     {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    // // Await the http get response, then decode the json-formatted response.
+    // final response = await http.get(url);
     final nowPlayingResponse =
-        NowPlayingResponse.fromJson(jsonDecode(response.body));
+        NowPlayingResponse.fromJson(jsonDecode(jsonData));
     //final Map<String, dynamic> decodedData = json.decode(response.body);
     //print(nowPlayingResponse.results[0].title);
     onDisplayMovies = nowPlayingResponse.results;
@@ -37,12 +49,15 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   getPopularMovies() async {
-    var url = Uri.https(_baseUrl, '3/movie/popular',
-        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+    _popularPage++;
 
-    // Await the http get response, then decode the json-formatted response.
-    final response = await http.get(url);
-    final popularResponse = PopularResponse.fromJson(jsonDecode(response.body));
+    final jsonData = await _getJsonData('3/movie/popular', _popularPage);
+    // var url = Uri.https(_baseUrl, '3/movie/popular',
+    //     {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    // // Await the http get response, then decode the json-formatted response.
+    // final response = await http.get(url);
+    final popularResponse = PopularResponse.fromJson(jsonDecode(jsonData));
     //final Map<String, dynamic> decodedData = json.decode(response.body);
     //print(nowPlayingResponse.results[0].title);
     popularMovies = [...popularMovies, ...popularResponse.results];
